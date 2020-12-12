b8515722fcb3401ee336dc14b612ac32d4ef59b0
Tim Siegler <tim.siegler@fincon.eu>
2020-12-12 02:07:19 +0100

    - Allowed post request

--- secret-santa-service/src/main/java/de/mrtimeey/secretsanta/SecurityConfiguration.java
+++ secret-santa-service/src/main/java/de/mrtimeey/secretsanta/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
