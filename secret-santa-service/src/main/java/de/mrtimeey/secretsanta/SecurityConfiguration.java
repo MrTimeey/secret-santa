48729884afcb84dea4a996a055b951cfd8763ad2
Tim Siegler <tim.siegler@fincon.eu>
2020-12-12 02:14:11 +0100

    - Allowed post request

--- secret-santa-service/src/main/java/de/mrtimeey/secretsanta/SecurityConfiguration.java
+++ secret-santa-service/src/main/java/de/mrtimeey/secretsanta/SecurityConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -29,16 +28,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()
-                .antMatchers("/api-docs/**").permitAll()
-                .antMatchers(HttpMethod.GET, "/**").permitAll()
-                .antMatchers(HttpMethod.POST, "/**").permitAll()
-                .antMatchers(HttpMethod.DELETE, "/**").permitAll()
-                .antMatchers(HttpMethod.PATCH, "/**").permitAll()
-                .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
-                .and()
+        http
                 .csrf().disable();
     }
 
--- secret-santa-service/src/main/java/de/mrtimeey/secretsanta/group/rest/controller/GroupController.java
+++ secret-santa-service/src/main/java/de/mrtimeey/secretsanta/group/rest/controller/GroupController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +35,7 @@ public class GroupController {
 
     @PostMapping
     @ResponseStatus( HttpStatus.CREATED )
+    @ResponseBody
     public SecretSantaGroupTO createGroup(@RequestBody @Valid CreateGroupRequest createGroupRequest) {
             return groupRestService.createNewGroup(createGroupRequest);
 
